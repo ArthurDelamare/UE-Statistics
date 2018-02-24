79cbba3392c44757a7299d1d5e55a9187ac87f99
Unknown <arthur.delamare1@viacesi.fr>
2018-02-24 23:27:25 +0100

    ADD Chapter AND UE Classes

---
+++ src/model/Chapter.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Chapter {
+
+	public String title = "";
+	public boolean completed = false;
+	public char grade = ' ';
+	public byte coefficient = 0; 
+	
+}
---
+++ src/model/UE.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class UE extends ArrayList<Chapter> {
+	
+	public void clean(){
+		System.out.println("Do a cleaning of the UE.");
+	}
+
+}
